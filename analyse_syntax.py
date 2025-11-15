12fec4c3a64648747085aa4f9dea0a2ffe9a6f90
Expert632 <machane.salim@gmail.com>
2025-11-15 19:51:13 +0100

    Update analyse_syntax.py

--- analyse_syntax.py
+++ analyse_syntax.py
@@ -1,16 +1,5 @@
 #!/usr/bin/env python3
 
-"""
-Script d'analyse de la vérification de syntaxe.
-
-Dans ce lab pédagogique, les étapes de compilation/syntaxe (Python, JS, Java)
-sont déjà exécutées dans le pipeline GitHub Actions.
-
-➡️ Si ce script est lancé, cela signifie que :
-   - aucune erreur de syntaxe n'a été détectée,
-   - donc on affiche simplement un message de succès.
-"""
-
 def main():
     message = "No problem with syntax"
     print(message)
